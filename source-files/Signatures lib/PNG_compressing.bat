b985d7ef9252b93f94aba92333f0a16f39f3ac5a
Nichita Sviridcenko <pp35697@gmail.com>
2025-09-12 12:36:29 +0300

    C optimized / x y z i added

---
+++ source-files/Signatures lib/PNG_compressing.bat
@@ -0,0 +1,20 @@
+@echo off
+:: Исходная папка
+set "SOURCE=C:\Users\Nichita Sviridcenko\Desktop\Signatures_backup\z"
+
+:: Папка назначения
+set "DEST=C:\Users\Nichita Sviridcenko\Zomboid\Workshop\Check-the-Papers\Contents\mods\Check-the-Papers\42\media\textures\signatures\z"
+
+:: Создать папку назначения, если её нет
+if not exist "%DEST%" mkdir "%DEST%"
+
+:: Цикл по всем PNG-файлам в исходной папке
+for %%F in ("%SOURCE%\*.png") do (
+    echo Обработка файла %%~nxF...
+
+    :: Применяем ImageMagick и сохраняем в DEST
+    magick "%%F" -colorspace Gray -define png:color-type=4 -strip "%DEST%\%%~nxF"
+)
+
+echo Готово!
+pause
